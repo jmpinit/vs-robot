b7fa1dadaa564218c62ba85b0a84f88208bbb1ae
jmptable <trueowen@verizon.net>
2013-01-11 16:27:46 -0500

    Gyro test inits gyro and then prints value over serial ever 100 ms.

--- robot/umain.c
+++ robot/umain.c
@@ -1,16 +1,23 @@
-// Include headers from OS
 #include <joyos.h>
 
-// usetup is called during the calibration period. 
+#define GYRO_PORT		8
+#define LSB_US_PER_DEG	1400000
+
 int usetup (void) {
+	printf("\nPlace robot,    press go.");
+	go_click ();
+	printf ("\nStabilizing...");
+	pause (500);
+	printf ("\nCalibrating     offset...\n");
+	gyro_init (GYRO_PORT, LSB_US_PER_DEG, 500L);
+
     return 0;
 }
 
-// Entry point to contestant code.
 int umain (void) {
     while (1) {
-		motor_set_vel(0, digital_read(0)*frob_read_range(0, 1023));
-		motor_set_vel(1, -digital_read(1)*frob_read_range(0, 1023));
+		printf ("\ntheta = %.2f", gyro_get_degrees());
+        pause (100);		
 	}
 
     return 0;
