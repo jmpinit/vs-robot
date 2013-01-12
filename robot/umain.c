0f4adc04eaeab9d3894542e30ce6d4c93de2b7d5
jmptable <trueowen@verizon.net>
2013-01-12 15:47:22 -0500

    LED initialization moved to it's own function.

--- robot/umain.c
+++ robot/umain.c
@@ -3,6 +3,11 @@
 #define GYRO_PORT		8
 #define LSB_US_PER_DEG	1400000
 
+void led_init(void) {
+	//create outputs for LEDs
+	DDRD = (1<<PD2)|(1<<PD3)|(1<<PD4);
+}
+
 void led_set(unsigned char led, unsigned char state) {
 	if(led<3) {
 		if(state)
@@ -12,19 +17,12 @@ void led_set(unsigned char led, unsigned char state) {
 	}
 }
 
-int usetup (void) {
-	//create outputs for LEDs
-	DDRD = (1<<PD2)|(1<<PD3)|(1<<PD4);
-
-	encoder_reset(24);
-
+int usetup(void) {
     return 0;
 }
 
 int umain (void) {
-    while (1) {
-		printf("encoder=%d\n", encoder_read(24));
-		pause(100);
+    while(1) {
 	}
 
     return 0;
