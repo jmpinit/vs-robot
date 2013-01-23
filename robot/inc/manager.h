37c8f15e587da5292324a6a26150f09eb269c2fc
jmptable <trueowen@verizon.net>
2013-01-23 11:35:55 -0500

    Static map datastructure now changed to an array of territory structs including waypoint, mine, and capture points, and heading info for mine and capture points.

--- robot/inc/manager.h
+++ robot/inc/manager.h
@@ -1,14 +1,31 @@
 #ifndef _MANAGER_
 #define _MANAGER_
 
-typedef enum activity {
+#define TEAM_RED	0
+#define TEAM_BLUE	1
+
+#include "util_math.h"
+
+typedef struct {
+	pt center;
+	pt mine;
+	pt capture;
+	float heading_mine;
+	float heading_capture;
+} territory;
+
+extern unsigned char team;
+
+/*enum Activity {
 	CAPTURE,
 	DUMP,
 	EXPLORE,
 	MINE
-}
+};*/
 
 //pick the next 
-activity act_pick(void);
+//activity act_pick(void);
+void manager_explore(void);
+void manager_visit(void);
 
 #endif
--- robot/manager.c
+++ robot/manager.c
@@ -1,16 +1,19 @@
 #include <joyos.h>
 #include "inc/manager.h"
+#include "inc/sensors.h"
+#include "inc/control.h"
 #include "inc/util_math.h"
 
-bool team;
+unsigned char team;
 
-pt map[] = {
-	{ -1200, 10 },
-	{ -590, -1048 },
-	{ 620, -1048 },
-	{ 1200, 10 },
-	{ 620, 1048 },
-	{ -590, 1048}
+territory map[6] = {
+	//center		mine			capture			mine angle	capture angle
+	{{-1200, 10},	{-1688, 341},	{-1773, -355},	149.275,	27.25},
+	{{-590, -1048},	{-1165, -1322},	{-593, -1733},	-151.6,		83},
+	{{620, -1048},	{575, -1670}, 	{1180, -1360},	-93,		147.7},
+	{{1200, 10},	{1664, -343},	{1711, 388},	-32.8,		-151.9},
+	{{620, 1048},	{1110, 1300},	{555, 1697},	25.80,		-93.27},
+	{{-590, 1048},	{-555, 1634},	{-1200, 1370},	83.956,		-33.85}
 };
 
 /* scoring information */
@@ -19,10 +22,57 @@ enum {
 	DUMP,		//per ping pong ball in the center
 	EXPLORE,	//per explored territory
 	MINE		//per ping pong ball mined
-}
+};
 
 unsigned char points[4];
-points[CAPTURE] = 100;
-points[DUMP]	= 40;
-points[EXPLORE]	= 30;
-points[MINE]	= 40;
+
+void manager_init(void) {
+	points[CAPTURE] = 100;
+	points[DUMP]	= 40;
+	points[EXPLORE]	= 30;
+	points[MINE]	= 40;
+}
+
+void manager_explore(void) {
+	for(unsigned char id=0; id<6; id++) {
+		move_to_ptp(map[(id+2)%6].center.x, map[(id+2)%6].center.y, 245);
+	}
+
+	move_to_ptp(map[0].center.x, map[0].center.y, 245);
+}
+
+void manager_visit(void) {
+	for(unsigned char id=0; id<6; id++) {
+		territory this = map[(id+2)%6];
+		move_to_ptp(this.center.x, this.center.y, 96);
+
+		vps_update();
+		//nav_turn_to(angle_to_target(this.capture.x, this.capture.y)+180);
+		nav_turn_to(this.heading_capture);
+		//float dist = vps_to_encoder(distance(vps_x, vps_y, this.capture.x, this.capture.y));
+		nav_straight(44, -96);
+
+		for(unsigned int i=0; i<200; i++) {
+			motor_set_vel(MOTOR_CAPTURE, i);
+			pause(1);
+		}
+
+		for(unsigned int i=200; i>=0; i--) {
+			motor_set_vel(MOTOR_CAPTURE, i);
+			pause(1);
+		}
+
+		gate_open();
+		pause(1000);
+		gate_close();
+
+		nav_straight(44, 96);
+
+		/*move_to_ptp(map[id].mine.x, map[id].mine.y);
+		pause(3000);
+		move_to_ptp(map[id].center.x, map[id].center.y);
+		move_to_ptp(map[id].capture.x, map[id].capture.y);
+		pause(3000);
+		move_to_ptp(map[id].center.x, map[id].center.y);*/
+	}	
+}
