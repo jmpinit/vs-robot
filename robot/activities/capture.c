60bf9660ba50e3f08d71551b7195995b8e612cfa
jmptable <trueowen@verizon.net>
2013-01-27 11:09:52 -0500

    Mining and capture routines working, but without timeouts or retry intelligence.

--- robot/activities/capture.c
+++ robot/activities/capture.c
@@ -3,24 +3,56 @@
 #include "../inc/sensors.h"
 #include "../inc/activities/capture.h"
 
-#define MAX_SPEED      150
-#define TURN_ACCEL      20
+#define CAPTURE_CURRENT	10
 
-int currentspeed = 20;
-
-void capture(unsigned char territory) {
-    while (abs(currentspeed)<MAX_SPEED && vps_get_owner(territory)!=team) {
-        
+void capture(unsigned char id) {
+    /*while (abs(currentspeed)<MAX_SPEED && vps_get_owner(territory)!=team) {
         if(team==TEAM_RED)
             currentspeed+=TURN_ACCEL;
         else
             currentspeed -=TURN_ACCEL;
-        
         wheel_capture(currentspeed);
-        
         pause(100);
-        
-    }
-        
-        
+    }*/
+
+	printf("= capturing %d =\n", id);
+
+	go_territory(id, 96);
+
+	printf("at waypoint! turning...\n");
+
+	nav_turn_to(map[id].heading_capture);
+	printf("back facing capture!\n");
+	printf("approaching...\n");
+
+	//reverse until we hit something
+	nav_set_velocity(-96);
+	float current = 0;
+	float lastcurrent = 0;
+	for(;;) {
+		current = motor_get_current_avg();
+		printf("current=%f\n");	
+
+		if(current>CAPTURE_CURRENT && lastcurrent>CAPTURE_CURRENT) break;
+
+		lastcurrent = current;
+	} 
+	nav_stop();
+	nav_set_velocity(-(MIN_SPEED-4));		//press against capture gears
+
+	//attempt to capture
+	printf("capturing!\n");
+	for(unsigned int i=0; i<255; i++) {
+		motor_set_vel(MOTOR_CAPTURE, (team==TEAM_BLUE)? i: -i);
+		pause(5);
+	}
+
+	printf("spinning down\n");
+	for(int i=255; i>=0; i--) {
+		motor_set_vel(MOTOR_CAPTURE, (team==TEAM_BLUE)? i: -i);
+		pause(2);
+	}
+
+	printf("backing up\n");
+	nav_straight_stop(30, 96);
 }
--- robot/activities/mine.c
+++ robot/activities/mine.c
@@ -1,23 +1,36 @@
 #include <joyos.h>
 #include "../inc/activities/mine.h"
 
-#define MINE_SPEED 300 //in milliseconds
-#define TIMEOUT_TRIES 3
+#define TRIES		3
+#define SPEED		96
 
-void mine(unsigned char territory){
-    int timestried = 0;
-    int ballsremaining = 0;
-    
-    
-    while(vps_get_balls(territory)>0 && timestried<TIMEOUT_TRIES){
-        
-        ballsremaining = vps_get_balls(territory);
-        lever_down();
-        pause(MINE_SPEED);
-        lever_middle();
-        pause(MINE_SPEED);
-        if (ballsremaining==vps_get_balls(territory))timestried++;
-        
-    }
-    lever_up();
+void mine(unsigned char id) {
+	printf("= mining %d =\n", id);
+
+	go_territory(id, SPEED);
+
+	printf("at waypoint! turning...\n");
+
+	nav_turn_to(map[id].heading_mine);
+	printf("facing mine!\n");
+	printf("approaching...\n");
+
+	//forward until we hit something
+	nav_set_velocity(96);
+	while(!digital_read(CONTACT_LEFT) && !digital_read(CONTACT_RIGHT)) { NOTHING; yield(); }
+	nav_stop();
+
+	//attempt to mine
+	printf("mining!\n");
+	lever_middle();
+	for(int i=0; i<5; i++) {
+		lever_down();
+		pause(300);
+		lever_middle();
+		pause(300);
+	}
+	pause(500);
+
+	printf("backing up\n");
+	nav_straight_stop(30, SPEED);
 }
