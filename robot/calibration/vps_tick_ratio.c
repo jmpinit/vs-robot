4a4884fd6cf75da224e2b0577af8017aa309906f
jmptable <trueowen@verizon.net>
2013-01-27 11:16:16 -0500

    Improved vps calibration routines for getting points and getting unit relationships.

---
+++ robot/calibration/vps_tick_ratio.c
@@ -0,0 +1,28 @@
+int start = encoder_read_avg();
+
+vps_update();
+int startx = vps_x;
+int starty = vps_y;
+
+nav_set_velocity(96);
+while(encoder_read_avg()-start<64) {
+	NOTHING;
+	yield();
+}
+
+nav_stop();
+
+vps_update();
+int endx = vps_x;
+int endy = vps_y;
+
+int end = encoder_read_avg();
+int ticks = end-start;
+
+float dist = distance(startx, starty, endx, endy);
+
+float ratio = ticks/dist;
+
+printf("ticks=%d dist=%f ratio=%f", ticks, dist, ratio);
+
+halt();
