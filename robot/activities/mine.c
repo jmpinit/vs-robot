1456eeae132127d082f4e4d27fc6fa352ef924e9
jmptable <trueowen@verizon.net>
2013-01-28 18:15:55 -0500

    Working mine routine that uses contact switches to square the robot against the wall before mining.

--- robot/activities/mine.c
+++ robot/activities/mine.c
@@ -1,27 +1,49 @@
 #include <joyos.h>
 #include "../inc/activities/mine.h"
+#include "../inc/control.h"
 
-#define TRIES		3
-#define SPEED		96
+#define TRIES			3
+#define MINE_TIMEOUT	3	//time until failure (sec)
+#define MINE_SPEED		96
+
+void wall_square(void) {
+	//square ourselves
+	nav_off();
+	long start = get_time_us();
+	while(!(digital_read(CONTACT_LEFT) && digital_read(CONTACT_RIGHT)) && (get_time_us()-start<MINE_TIMEOUT*1000000)) {
+		if(digital_read(CONTACT_LEFT)) {
+			motor_set_vel(MOTOR_LEFT, -(MIN_SPEED+10));
+			motor_set_vel(MOTOR_RIGHT, 64);
+		}
+		if(digital_read(CONTACT_RIGHT)) {
+			motor_set_vel(MOTOR_RIGHT, -(MIN_SPEED+10));
+			motor_set_vel(MOTOR_LEFT, 64);
+		}
+	}
+	nav_on();
+}
 
 void mine(unsigned char id) {
-	printf("= mining %d =\n", id);
+	PRINT("= mining %d =\n", id);
 
-	go_territory(id, SPEED);
+	go_territory(id, MINE_SPEED);
 
-	printf("at waypoint! turning...\n");
+	PRINT("at waypoint! turning...\n");
 
-	nav_turn_to(within(-180, angle_between(bot.x, bot.y, arena[id].mine.x, arena[id].mine.y), 180));
-	printf("facing mine!\n");
-	printf("approaching...\n");
+	nav_turn_to(within(-180, angle_between(bot.x, bot.y, arena[id].mine.x, arena[id].mine.y)+arena[id].heading_mine, 180));
+	PRINT("facing mine!\n");
+	PRINT("approaching...\n");
 
 	//forward until we hit something
-	nav_set_velocity(96);
-	while(!digital_read(CONTACT_LEFT) && !digital_read(CONTACT_RIGHT)) { NOTHING; yield(); }
+	nav_set_velocity(MINE_SPEED);
+	float start = get_time_us();
+	while(!digital_read(CONTACT_LEFT) && !digital_read(CONTACT_RIGHT) && (get_time_us()-start<MINE_TIMEOUT*1000000)) { NOTHING; yield(); }
 	nav_stop();
 
+	wall_square();
+
 	//attempt to mine
-	printf("mining!\n");
+	PRINT("mining!\n");
 	lever_middle();
 	for(int i=0; i<5; i++) {
 		lever_down();
@@ -31,6 +53,8 @@ void mine(unsigned char id) {
 	}
 	pause(500);
 
-	printf("backing up\n");
-	nav_straight_stop(30, SPEED);
+	PRINT("backing up\n");
+	nav_straight(30, -MINE_SPEED);
+
+	lever_up();
 }
